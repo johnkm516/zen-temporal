882a2489b2dd3f50a5761fa5b759c193f2cd8add
John Kim <johnk419@hotmail.com>
2023-04-12 09:21:20 +0900

    Modified temporalHost to localhost for dev

--- apps/temporal-worker/src/environments/environment.ts
+++ apps/temporal-worker/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { EnvironmentBase } from './environment.base';
 
 export const environment: EnvironmentBase = {
   production: false,
-  temporalHost: '192.168.0.168:7233',
+  temporalHost: 'localhost:7233',
   API_URL: process.env.API_URL,
   API_PASSWORD: process.env.API_PASSWORD,
   API_USERNAME: process.env.API_USERNAME,
